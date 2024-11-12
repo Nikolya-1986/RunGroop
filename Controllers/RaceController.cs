916704aa6b61cd02009d1a92140a2a0c6fb09790
Nikolai Amosov <nikolai_sergeevich@mail.ru>
2024-11-12 23:23:11 +0300

    create race

--- Controllers/RaceController.cs
+++ Controllers/RaceController.cs
@@ -22,5 +22,21 @@ namespace RunGroop.Controllers
             Race race = await _raceRepository.GetByIdAsync(id);
             return View(race);
         }
+
+            public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Race race)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(race);
+            }
+            _raceRepository.Add(race);
+            return RedirectToAction("Index");
+        }
     }
 }
